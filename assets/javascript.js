a3429a2e1b1e4d30a20ef693cef7d4181b59a99c
David <dgibbs8089@gmail.com>
2020-09-29 17:50:26 -0500

    changed navbar to pages and removed aboutme section

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,3 +1,5 @@
+/* javascript for nav trigger functionality*/
+
 $('.navTrigger').click(function () {
     $(this).toggleClass('active');
     console.log("Clicked menu");
@@ -5,3 +7,31 @@ $('.navTrigger').click(function () {
     $("#mainListDiv").fadeIn();
 
 });
+
+/* contact form animations */
+
+$('.contact-form').find('.form-control').each(function() {
+    var targetItem = $(this).parent();
+    if ($(this).val()) {
+        $(targetItem).find('label').css({
+        'top': '10px',
+        'fontSize': '14px'
+        });
+    }
+    })
+    $('.contact-form').find('.form-control').focus(function() {
+    $(this).parent('.input-block').addClass('focus');
+    $(this).parent().find('label').animate({
+        'top': '10px',
+        'fontSize': '14px'
+    }, 300);
+    })
+    $('.contact-form').find('.form-control').blur(function() {
+    if ($(this).val().length == 0) {
+        $(this).parent('.input-block').removeClass('focus');
+        $(this).parent().find('label').animate({
+        'top': '25px',
+        'fontSize': '18px'
+        }, 300);
+    }
+    })
